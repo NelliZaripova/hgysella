fd2771197549cc7126c8c4dbc67bbf848d220268
Harra Gysella goodbrother <hgysella@mi-d6.21-school.ru>
2019-05-09 17:13:36 +0300

    add projects 21

---
+++ gnl/get_next_line.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgysella <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/06 15:52:07 by hgysella          #+#    #+#             */
+/*   Updated: 2018/12/11 11:42:19 by hgysella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+
+size_t	index_search(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+void	ft_read_fd(char **s, int fd)
+{
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
+	char	*tfd;
+
+	if (!*s)
+		*s = ft_strnew(BUFF_SIZE);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		tfd = ft_strjoin(*s, buff);
+		if (!tfd)
+		{
+			free(tfd);
+			ft_strdel(s);
+			break ;
+		}
+		ft_strdel(s);
+		*s = tfd;
+		if (ft_strchr(*s, '\n') != NULL)
+			break ;
+	}
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char		**str;
+	size_t			i;
+	char			*tmp;
+
+	if (fd < 0 || !line || fd >= FD_MAX || read(fd, NULL, 0) < 0)
+		return (-1);
+	if (!str)
+		str = (char **)malloc(sizeof(char *) * (FD_MAX + 1));
+	ft_read_fd(&str[fd], fd);
+	if (str[fd])
+	{
+		i = index_search(str[fd]);
+		if (i == 0 && str[fd][0] != '\n')
+			return (0);
+		*line = ft_strsub(str[fd], 0, i);
+		if (i == ft_strlen(str[fd]))
+			tmp = ft_strdup("");
+		else
+			tmp = ft_strsub(str[fd], i + 1, ft_strlen(str[fd]));
+		ft_strdel(&str[fd]);
+		str[fd] = tmp;
+		return (1);
+	}
+	return (-1);
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgysella <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 16:32:21 by hgysella          #+#    #+#             */
+/*   Updated: 2018/12/11 11:55:54 by hgysella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 10
+# define FD_MAX 11000
+
+int		get_next_line(const int fd, char **line);
+
+#endif
